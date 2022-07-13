d8d7f0ea3fb75b110f7f2bd2e5d0b870f40b0f94
Garnik Hovsepyan <garoh2291@gmail.com>
2022-07-13 17:21:56 +0400

    changes

---
+++ src/schemas/task.schema.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose'),
+  mongoosePaginate = require('mongoose-paginate'),
+  Schema = mongoose.Schema,
+  ObjectId = Schema.Types.ObjectId;
+
+const TaskSchema = new Schema(
+  {
+    owner: {
+      type: ObjectId,
+      ref: 'User',
+      required: true
+    },
+      title: {
+      type: String,
+      required: true
+    },
+    status: {
+      type: String,
+      enum: ["doing", "done","to do"]
+    },
+    date: {
+      type: Date,
+        format : 'date-time'
+    },
+  },
+  {
+    timestamps: {
+      createdAt: 'created_at',
+      updatedAt: 'updated_at'
+    }
+  }
+);
+
+TaskSchema.plugin(mongoosePaginate);
+
+module.exports = mongoose.model('Task', TaskSchema);
